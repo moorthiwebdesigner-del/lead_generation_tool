234b74ea580cc2cb6d8c63226351fd7331615148
Moorthi <moorthiwebdesigner@gmail.com>
2026-08-04 08:36:25 +0530

    Update production API URL

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://lead-generation-api-2sny.onrender.com",
 });
 
 export default api;
