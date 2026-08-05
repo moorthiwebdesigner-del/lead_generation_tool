e5f86096bf5a53cc9895e843c224f908b8916862
Moorthi <moorthiwebdesigner@gmail.com>
2026-08-05 11:27:52 +0530

    Fix logo display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,65 +1,181 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom";
+
+
+import Layout from "./layout/Layout";
+
+
+import Dashboard from "./pages/Dashboard";
+import Search from "./pages/Search";
+import SavedLeads from "./pages/SavedLeads";
+import Analytics from "./pages/Analytics";
+import Settings from "./pages/Settings";
+import Login from "./pages/Login";
+
+
+import InstallPopup from "./components/InstallPopup";
+
+
+import { useAuth } from "./context/AuthContext";
+
+
+
 function App() {
+
 
   const { token } = useAuth();
 
 
+
   return (
+
     <BrowserRouter>
 
-      {/* Install Popup - All Pages */}
+
+      {/* PWA Install Popup - All Pages */}
+
       <InstallPopup />
+
 
 
       <Routes>
 
+
+
+        {/* Login */}
+
         <Route
+
           path="/login"
+
           element={
-            token 
-            ? 
+
+            token
+
+            ?
+
             <Navigate to="/" />
+
             :
+
             <Login />
+
           }
+
         />
 
 
+
+
+
+        {/* Protected Routes */}
+
         <Route
+
+
           element={
+
             token
+
             ?
+
             <Layout />
+
             :
+
             <Navigate to="/login" />
+
           }
+
+
         >
 
-          <Route path="/" element={<Dashboard />} />
 
-          <Route path="/search" element={<Search />} />
 
-          <Route 
-            path="/saved-leads" 
-            element={<SavedLeads />} 
+          <Route
+
+            path="/"
+
+            element={<Dashboard />}
+
           />
 
-          <Route 
-            path="/analytics" 
-            element={<Analytics />} 
+
+
+
+          <Route
+
+            path="/search"
+
+            element={<Search />}
+
           />
 
-          <Route 
-            path="/settings" 
-            element={<Settings />} 
+
+
+
+          <Route
+
+            path="/saved-leads"
+
+            element={<SavedLeads />}
+
           />
+
+
+
+
+          <Route
+
+            path="/analytics"
+
+            element={<Analytics />}
+
+          />
+
+
+
+
+          <Route
+
+            path="/settings"
+
+            element={<Settings />}
+
+          />
+
+
 
         </Route>
 
 
+
+        {/* Unknown URL */}
+
+        <Route
+
+          path="*"
+
+          element={<Navigate to="/" />}
+
+        />
+
+
+
       </Routes>
 
+
+
     </BrowserRouter>
+
   );
+
 }
+
+
 
 export default App;
